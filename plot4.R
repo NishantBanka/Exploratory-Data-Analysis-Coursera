93f75e53d41a6985db9abe5efd40180184f35f2e
NishantBanka <nishantbanka@gmail.com>
2017-06-22 10:34:00 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -2,6 +2,7 @@ hpc <- read.table("household_power_consumption.txt",sep=";",stringsAsFactors = F
 hpc <- subset(hpc,hpc$V1=='1/2/2007' | hpc$V1=='2/2/2007')
 colnames(hpc)<- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 hpc$TimeStamp <- paste(hpc$Date,hpc$Time)
+png(filename="plot4.png")
 par(mfrow=c(2,2))
 plot(strptime(hpc$TimeStamp,"%d/%m/%Y %H:%M:%S"),hpc$Global_active_power,type='l',ylab="Global Active Power(kilowatts)",xlab="")
 
@@ -13,3 +14,4 @@ lines(strptime(hpc$TimeStamp,"%d/%m/%Y %H:%M:%S"),hpc$Sub_metering_3,type='l',co
 legend("topright",lty=1,col=c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex=0.5)
 
 plot(strptime(hpc$TimeStamp,"%d/%m/%Y %H:%M:%S"),hpc$Global_reactive_power,type='l',ylab="Global_reactive_power",xlab="datetime")
+dev.off()
